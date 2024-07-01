7a4ff7068358890ccce88612a15c39fce8d46360
Kevin Cruz <devs-dgrv@ip.gob.hn>
2024-07-01 11:18:38 -0600

    inicio

---
+++ src/main/java/com/example/Foro_Hub/ForoHubApplication.java
@@ -0,0 +1,15 @@
+package com.example.Foro_Hub;
+
+import org.flywaydb.core.internal.util.JsonUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
